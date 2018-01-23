66e0c844998608da659dd7c646dc12b3a2a4f6c1
winnyclaudia <32456592+winnyclaudia@users.noreply.github.com>
2018-01-22 17:43:27 -0800

    selesai

--- app/src/main/java/com/kharisma/androidcafe/DetailActivity.java
+++ app/src/main/java/com/kharisma/androidcafe/DetailActivity.java
@@ -1,14 +1,25 @@
 package com.kharisma.androidcafe;
 
-
 import android.app.FragmentManager;
-import android.support.v4.app.Fragment;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
-
-import android.support.annotation.Nullable;
 import android.os.Bundle;
 
+import android.support.annotation.Nullable;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+
 public class DetailActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -18,10 +29,48 @@ public class DetailActivity extends AppCompatActivity {
         // Retrive the data coming from MainActivity.java
         Menu menu = getIntent().getParcelableExtra("menu");
 
-        // Pass the data to DetailFragmentB.java to display it
+
         FragmentManager fragmentManager = getFragmentManager();
         FragmentDetail detailFragment = (FragmentDetail) fragmentManager.findFragmentById(R.id.fragmentD);
         detailFragment.displayData(menu);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Detail Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
 }
--- app/src/main/java/com/kharisma/androidcafe/FragmentDetail.java
+++ app/src/main/java/com/kharisma/androidcafe/FragmentDetail.java
@@ -1,10 +1,8 @@
 package com.kharisma.androidcafe;
 
-import android.content.Context;
-import android.net.Uri;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
